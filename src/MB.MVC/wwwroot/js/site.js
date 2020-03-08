7272d1b61a64c05fcbcb8e7e9e70bea088a7a9fb
Michael B <mjbradvica@gmail.com>
2020-03-08 13:52:48 -0400

    css fiddles

--- src/MB.MVC/wwwroot/js/site.js
+++ src/MB.MVC/wwwroot/js/site.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     document.addEventListener("wheel", function (event) {
         if (event.deltaY > 0) {
-            window.scrollBy(0, vh);
+            window.scrollBy(0, viewHeight);
         }
         else {
-            window.scrollBy(0, -vh);
+            window.scrollBy(0, -viewHeight);
         }
     });
 }, false);
+var darkMode = function () {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+};
 //# sourceMappingURL=site.js.map
