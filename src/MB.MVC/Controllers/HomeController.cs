e5dafaf451c2cdcca92dd54853f3f77f41259f14
Michael Bradvica <michael.bradvica@motorolasolutions.com>
2020-11-25 13:01:29 -0800

    swap to pure

--- src/MB.MVC/Controllers/HomeController.cs
+++ src/MB.MVC/Controllers/HomeController.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using MB.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MB.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HomeViewModel _viewModel;
+
+        public HomeController()
+        {
+            var blogPosts = new List<string>
+            {
+                "FixOneLineEveryDay",
+            };
+
+            _viewModel = new HomeViewModel(blogPosts);
+        }
+
         public IActionResult Index()
+        {
+            return View(_viewModel);
+        }
+
+        public IActionResult About()
         {
             return View();
         }
---
+++ src/MB.MVC/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MB.MVC.Models
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel(IEnumerable<string> postPartials)
+        {
+            PostPartials = postPartials;
+        }
+
+        public IEnumerable<string> PostPartials { get; }
+    }
+}
