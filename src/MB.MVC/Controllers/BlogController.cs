4782e1a0fea6cdbdc65013204c114fd85e56a432
Michael Bradvica <mjbradvica@protonmail.com>
2021-03-23 14:32:36 -0700

    layout and update to about

--- src/MB.MVC/Controllers/BlogController.cs
+++ src/MB.MVC/Controllers/BlogController.cs
@@ -5,9 +5,6 @@ namespace MB.MVC.Controllers
     public class BlogController : Controller
     {
         [Route("one-line-a-day")]
-        public IActionResult OneLineADay()
-        {
-            return View();
-        }
+        public IActionResult OneLineADay() => View();
     }
 }
