4d723aae15103e0cadc3a08a17d2cead0b461f57
Michael B <mjbradvica@protonmail.com>
2021-03-31 11:28:26 -0700

    updated articles and added chain

--- src/MB.MVC/Controllers/BlogController.cs
+++ src/MB.MVC/Controllers/BlogController.cs
@@ -6,5 +6,8 @@
     {
         [Route("one-line-a-day")]
         public IActionResult OneLineADay() => View();
+
+        [Route("chain-of-responsibility-adopted-for-dependency-injection")]
+        public IActionResult ChainOfResponsibility() => View();
     }
 }
